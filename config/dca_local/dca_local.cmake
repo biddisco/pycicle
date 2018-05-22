570a7234b0a761c9e42dbf86244e924ae0c35378
Peter Doak <doakpw@ornl.gov>
2018-05-22 11:34:37 -0400

    necessary changes for running on non-org repo
    also config to run on alternate cdash, although that still doesn't work

--- config/dca_local/dca_local.cmake
+++ config/dca_local/dca_local.cmake
@@ -1,17 +1,17 @@
-#  Copyright (c) 2018      Peter Doak
-#  Copyright (c) 2017-2018 John Biddiscombe
+#  Copyright (c) 2017-2018 John Biddiscombe, Peter Doak
 #
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 # Github settings
 set(PYCICLE_GITHUB_PROJECT_NAME  "PDoakORNL/DCA")
-set(PYCICLE_GITHUB_ORGANISATION  "")
-set(PYCICLE_GITHUB_MASTER_BRANCH "master")
+set(PYCICLE_GITHUB_ORGANISATION  "PDoakORNL")
+set(PYCICLE_GITHUB_MASTER_BRANCH "gpu_trunk")
+set(PYCICLE_HTTP TRUE)
 
 # CDash server settings
 set(PYCICLE_CDASH_PROJECT_NAME "DCA")
-set(PYCICLE_CDASH_SERVER_NAME  "localhost")
+set(PYCICLE_CDASH_SERVER_NAME  "localhost:38080")
 set(PYCICLE_CDASH_HTTP_PATH    "cdash")
 
 # project specific target to build before running tests
