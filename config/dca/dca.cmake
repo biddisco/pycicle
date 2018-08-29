9beadfc229013672922dc13414861cb89e8d399e
John Biddiscombe <biddisco@cscs.ch>
2018-08-29 15:52:51 +0200

    DCA repo has moved to CompFUSE organisation

--- config/dca/dca.cmake
+++ config/dca/dca.cmake
@@ -5,8 +5,8 @@
 
 # Github settings
 set(PYCICLE_GITHUB_PROJECT_NAME  "DCA")
-set(PYCICLE_GITHUB_ORGANISATION  "eth-cscs")
-set(PYCICLE_GITHUB_BASE_BRANCH "master")
+set(PYCICLE_GITHUB_ORGANISATION  "CompFUSE")
+set(PYCICLE_GITHUB_BASE_BRANCH   "master")
 
 # CDash server settings
 set(PYCICLE_CDASH_PROJECT_NAME   "DCA")
